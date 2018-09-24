cf3c50f713f845c8bb94baa454def12d390e21f9
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-09-24 10:58:22 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,15 @@
 <?php
+session_start();
 
+$foto = $_FILES['file']['name'];
+$tmp_foto = $_FILES['file']['tmp_name'];
+$dir = 'foto/';
+$target = $dir.$foto;
+
+$_SESSION['hobby'] = $_POST['hobby'];
+$_SESSION['file'] = $target;
+if(!move_uploaded_file($tmp_foto, $target)){
+    die("gagal upload");
+}
+header("location:halamanakhir.php");
 ?>
