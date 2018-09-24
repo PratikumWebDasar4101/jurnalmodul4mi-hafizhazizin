48cdf34f0272db64b35b7df19e4857137bdbca87
Hafizh 'Azizi Nasution <42725297+hafizhazizin@users.noreply.github.com>
2018-09-24 10:59:05 +0700

    Update halaman akhir.php

--- halaman akhir.php
+++ halaman akhir.php
@@ -1,1 +1,23 @@
-halaman akhir bisa diperlukan
+<?php
+session_start();
+?>
+    <table border='1' align="center" width="800px" >
+        <tr>
+            <th>Nama</th>
+            <th>Hobi</th>
+            <th>Gambar</th>
+        </tr>
+        <tr>
+            <td>
+            <?php
+                foreach($_SESSION['hobby'] as $pilihan){
+                echo $pilihan ."<br>";
+             }
+            ?>
+        </td>
+            <td align="center">
+                <img src="<?php echo $_SESSION['file'];?>" width ="50%">
+            </td>
+        </tr>
+    </table>
+    <a href="form.php">Hapus ??</a>
